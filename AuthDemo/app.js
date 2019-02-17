b94af585a4df1e1a7374bf8d00d877b026cd9fb5
Troykaka666 <troywu9527@gmail.com>
2019-02-17 22:26:15 +0000

    set up authentication (login, signup and logout) by using passport.js

--- AuthDemo/app.js
+++ AuthDemo/app.js
@@ -1,18 +1,90 @@
 var express = require("express"),
     app = express(),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    User = require("./models/user"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
     mongoose = require("mongoose");
     
-mongoose.connect("mongodb://localhost/auth_demo_app");
+mongoose.connect("mongodb://localhost/auth_demo_app", { useNewUrlParser: true });
     
 app.set('view engine', 'ejs');
+
+
+//This code is required whenever we want to retrive date from a post
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(require("express-session")({
+    secret: "My cats are the cutest animal in the world",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//====================
+// ROUTES
+//===================
 
 app.get("/", function(req, res){
    res.render("home"); 
 });
 
-app.get("/secret", function(req, res){
+app.get("/secret", isLoggedIn, function(req, res){
     res.render("secret");
 });
+
+//Auth Route
+app.get("/register", function(req, res) {
+   res.render("register"); 
+});
+app.post("/register", function(req, res){
+    //1. create a user first by passing the username
+    //2. Adding a new use by using register to the mongoose DB
+   User.register(new User({username: req.body.username}), req.body.password, function(err, user){
+       if(err){
+           console.log(err);
+           return res.render('register');
+       }
+       passport.authenticate("local")(req, res, function(){
+           res.redirect("/secret");
+       });
+   });
+});
+
+//LOGIN ROUTES
+//rederlogin form
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+//login logic: middleware
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/login"
+}), function(req, res){
+    
+});
+
+
+//Logout routes
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server starting.....");
--- redme.md
+++ redme.md
@@ -3,3 +3,22 @@ Passport.js
 Passport Local
 Passport Local Mongoose
 
+1.
+* Set up mongodb and express.js structure
+* Add Secret route and template
+
+2.
+*Create User model
+*Configure passport
+
+3.
+*Add Register routes
+*Add Register form
+
+4.
+*Add Login routes
+*Add Login form
+
+5.
+*Add Logout Route
+*Add isLoggedIn middleware
